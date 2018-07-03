9e3f7ce60cb9bed65e9f5bd707807a83ef448c63
Jorge Hernández <r3b0rN.jhs@gmail.com>
2018-07-03 13:50:29 +0100

    Refactor: it_should_parse_to_int_when_String_have_one_element

--- src/Tests.java
+++ src/Tests.java
@@ -7,10 +7,11 @@ class Tests{
     @Test
     void it_should_parse_to_int_when_String_have_one_element(){
         assertEquals(sumNumbersFrom("1"), 1);
+        assertEquals(sumNumbersFrom("2"), 2);
     }
 
     private int sumNumbersFrom(String expression) {
-        return 1;
+        return Integer.parseInt(expression);
     }
 
 
