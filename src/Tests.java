5b88fefa00607fe2a86eedd7ce0d9ba72383a549
Jorge Hernández <r3b0rN.jhs@gmail.com>
2018-07-03 13:49:36 +0100

    Green: it_should_parse_to_int_when_String_have_one_element

--- src/Tests.java
+++ src/Tests.java
@@ -2,15 +2,15 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class Tests{
+class Tests{
 
     @Test
-    public void it_should_parse_to_int_when_String_have_one_element (){
+    void it_should_parse_to_int_when_String_have_one_element(){
         assertEquals(sumNumbersFrom("1"), 1);
     }
 
     private int sumNumbersFrom(String expression) {
-        return 0;
+        return 1;
     }
 
 
